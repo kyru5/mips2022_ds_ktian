caa339f60a3516d947c05385d31ce80a6cb72f1c
kyru5 <keyreel.tian@gmail.com>
2022-09-27 01:19:17 +0300

    test

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ reg = [('Ivanov', 'Sergej', 24, 9, 1995),
       ('Smith', 'John', 13, 2, 2003),
       ('Petrova', 'Maria', 13, 3, 2003)]
 
-new_reg = map(lambda x: (x[0] + ' ' + x[1][0] + '.', x[2], x[3], x[4]), filter(lambda x: x[-1] >= 2000, reg))
+new_reg = map(lambda x: (x[0] + ' ' + x[1][0] + '.', x[2], x[3], x[]), filter(lambda x: x[-1] >= 2000, reg))
 
 print(list(new_reg))
-
